7a4abad1b1304babdd2b2294da9b7e250495d323
FuadTalukder85 <webfuadtk@gmail.com>
2025-03-08 00:42:08 +0600

    working on testemonials

---
+++ js/testimonials.js
@@ -0,0 +1,72 @@
+function loadtestimonials() {
+  const container = document.getElementById("testimonialsContainer");
+  if (!container) {
+    console.error("Testimonials container not found");
+    return;
+  }
+  const testimonials = [
+    {
+      rating: 5,
+      review:
+        "Lorem ipsum dolor sit amet, consectetur ad-zzz-ing elit. Proin vel urna at metus tempor mattis, or not, whatever.",
+      name: "X_AE_A_13",
+      designation: "Product Designer, slothUI",
+    },
+    {
+      rating: 4,
+      review:
+        "Posuere una nec tincidunt praesent semper feugiat nibh sed. Velit dignissim sodales ut eu.",
+      name: "Azunyan U. Wu",
+      designation: "CEO, nextlife.ai",
+    },
+    {
+      rating: 5,
+      review:
+        "Sit amet est placerat in. Blandit cursus risus at ultrices mi tempus imperdiet. Volutpat commodo sed egestas egestas fringilla phasellus. Tincidunt eget nullam non nisi.",
+      name: "Mechatronics Yi",
+      designation: "CTO, fin4win.ai",
+    },
+    {
+      rating: 5,
+      review:
+        "Nisi porta lorem mollis aliquam ut porttitor leo. Sed odio morbi quis commodo. Arcu non odio euismod lacinia at quis. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae.",
+      name: "Oarack Babama",
+      designation: "Former President of US",
+    },
+    {
+      rating: 5,
+      review:
+        "Accumsan tortor posuere ac ut consequat. Quis varius quam quisque id diam vel. Commodo odio aenean sed adipiscing diam donec adipiscing. Vel pharetra vel turpis nunc eget.",
+      name: "Saylor Twift",
+      designation: "Famous Singer",
+    },
+    {
+      rating: 3,
+      review:
+        "Venenatis cras sed felis eget velit aliquet. Velit scelerisque in dictum non consectetur. Felis eget velit aliquet sagittis. Ultricies integer quis auctor elit sed vulputate mi.",
+      name: "Asuna Yuuki",
+      designation: "Virtual Swordsman",
+    },
+    {
+      rating: 2,
+      review:
+        "Sagittis eu volutpat odio facilisis mauris sit amet massa. Urna et pharetra pharetra massa.",
+      name: "Boe Jiden",
+      designation: "Former President Of Canada",
+    },
+    {
+      rating: 5,
+      review:
+        "Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Nec nam aliquam sem et tortor consequat id porta nibh. Viverra suspendisse. Lacus sed viverra tellus in hac habitasse platea dictumst. Elit at imperdiet dui accumsan.",
+      name: "Vermillion D. Gray",
+      designation: "CEO, hacklife.ai",
+    },
+    {
+      rating: 4,
+      review:
+        "Viverra accumsan in nisl nisi scelerisque eu. Vitae congue eu consequat ac felis donec.",
+      name: "Zuckman Wu",
+      designation: "CEO, tech4life.ai",
+    },
+  ];
+}
