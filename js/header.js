ce1f7aa09540a2674a1f7d40f2cb34a71c6858d4
Fuad Talukder <webfuadtk@gmail.com>
2025-03-10 11:42:47 +0600

    make button hover effect

--- js/header.js
+++ js/header.js
@@ -10,6 +10,12 @@ function loadMenu() {
     <ul class="res-list">
       <li id="closeMenu" class="close-menu"><i class="fa-solid fa-xmark"></i></li>
       ${menus.map((menu) => `<li style="padding: 10px;">${menu}</li>`).join("")}
+      <li>
+        <div class="header-res">
+          <button class="btn-outline">Sign Up</button>
+          <button class="btn-fill">Log In</button>
+        </div>
+      </li>
     </ul>
   `;
   container.innerHTML = menuHTML;
